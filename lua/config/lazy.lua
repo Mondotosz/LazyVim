2a699d4ccffc5b0031e9baf1842a047b27200c66
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2024-04-06 23:27:06 +0200

    added lazyrc, yaml schemas and set prettier as the markdown formatter

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "lazyvim.plugins.extras.lazyrc" }, -- must be the last thing
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,9 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "yaml-language-server",
+      "json-lsp",
+    },
+  },
+}
