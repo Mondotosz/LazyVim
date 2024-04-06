8c54fbe333e7b3df9186408dc6bae662f0fe402f
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2024-04-06 23:49:54 +0200

    added lualine configuration for yaml with schema

--- lua/plugins/yaml-companion.lua
+++ lua/plugins/yaml-companion.lua
@@ -1,12 +1,37 @@
 return {
-  "someone-stole-my-name/yaml-companion.nvim",
-  ft = "yaml",
-  dependencies = {
-    { "neovim/nvim-lspconfig" },
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-telescope/telescope.nvim" },
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    ft = "yaml",
+    dependencies = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function(_, opts)
+      local cfg = require("yaml-companion").setup(opts)
+      require("lspconfig")["yamlls"].setup(cfg)
+      require("telescope").load_extension("yaml_schema")
+    end,
   },
-  config = function()
-    require("telescope").load_extension("yaml_schema")
-  end,
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.sections.lualine_z = {
+        {
+          function()
+            local schema = require("yaml-companion").get_buf_schema(0)
+            if schema.result[1].name == "none" then
+              return ""
+            end
+            return schema.result[1].name
+          end,
+        },
+        {
+          function()
+            return " " .. os.date("%R")
+          end,
+        },
+      }
+    end,
+  },
 }
