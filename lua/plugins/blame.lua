818fa3c18547265307c196df12ae24a6298f1a3d
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2024-04-16 21:36:07 +0200

    updated plugins and fixed blame.nvim keybinds

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -1,7 +1,7 @@
 return {
   "FabijanZulj/blame.nvim",
   keys = {
-    { "<leader>gb", "<cmd>ToggleBlame virtual<cr>", desc = "[G]it [B]lame toggle" },
+    { "<leader>gb", "<cmd>BlameToggle<cr>", desc = "[G]it [B]lame toggle" },
   },
   opts = {
     merge_consecutive = true,
